a31fb7051df26190acde4bfa5a582d5af9bde608
tester313243 <4belstu@mail.ru>
2021-10-22 00:06:56 +0300

    Second LAB

---
+++ OOP_3_SEM/LAB_3/Airplane.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_3
+{
+  
+    partial class Airplane
+    {
+        public string Place { get; set; }
+        public string Plane_Type { get; set; }
+        public uint Plane_number { get; set; }
+        public string Time { get; set; }
+        public DayOfWeek Day { get; set; }
+        public int ID { get;}
+
+        public const int CONST_VALUE = 666;
+
+        public static int Object_Count = 0;
+        public Airplane()
+        {
+            Place = "NO PLace";
+            Plane_Type = "NO PLane";
+            Plane_number = 0;
+            Time = "NO Time";
+            Day = DayOfWeek.Never;
+            Object_Count++;
+        }
+        public Airplane(string Place, string Plane_Type, uint Plane_number, string Time, DayOfWeek Day)
+        {
+            this.Place = Place;
+            this.Plane_Type = Plane_Type;
+            this.Plane_number = Plane_number;
+            this.Time = Time;
+            this.Day = Day;
+            ID = rnd.Next(0, 1000);
+            Object_Count++;
+        }
+        public Airplane( string Plane_Type = "C-111", uint Plane_number = 1, string Time ="19:00", DayOfWeek Day = DayOfWeek.Everyday)
+        {
+           
+            this.Plane_Type = Plane_Type;
+            this.Plane_number = Plane_number;
+            this.Time = Time;
+            this.Day = Day;
+            ID = rnd.Next(0, 1000);
+            Object_Count++;
+        }
+        static Airplane()
+        {
+            Console.WriteLine("Вот информация о вашем рейсе:\n");
+        }
+        private Airplane(string Error)
+        {
+            Place = Error;
+        }
+
+    }
+    enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+        Never,
+        Everyday
+
+    }
+}
---
+++ OOP_3_SEM/LAB_3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Airplane[] airplane = new Airplane[3];
+
+
+            airplane[0] = new Airplane("Minsk", "SU - 132", 121, "12:00", DayOfWeek.Monday);
+            airplane[1] = new Airplane("Brest", "SU - 112", 101, "19:00", DayOfWeek.Friday);
+            airplane[2] = new Airplane("Mogilev", "BU - 32", 99, "22:00", DayOfWeek.Sunday);
+
+            for (int i = 0; i < airplane.Length; i++)
+            {
+                airplane[i].Print();
+            }
+
+           /* airplane1.Print();
+            airplane2.Print();
+            airplane3.Print();
+*/
+
+            // Console.WriteLine(airplane1.ID);
+            var airplane4 = new {S = "Mogilev", M = "BU - 32",T = 99, A = "22:00", DayOfWeek.Sunday };
+            Console.WriteLine(airplane4);
+            Console.WriteLine(Airplane.Object_Count);
+
+            int Out_value;
+            Funct_Out(out Out_value);
+            int Ref_value = 999;
+            Funct_Ref(ref Ref_value);
+        }
+        static void Funct_Out(out int x)
+        {
+            x = 11;
+        }
+        static void Funct_Ref(ref int x)
+        {
+            x = 2331;
+        }
+
+
+        
+
+    }
+   
+    partial class Airplane
+    {
+        Random rnd = new Random();
+        public void Print()
+        {
+            Console.WriteLine($"Время вылета: {Time}\nДень вылета: {Day}\nМесто приземления: {Place}\nНомер самолёта: {Plane_number }\nТип Самолета: {Plane_Type}\n");
+
+        }
+        public override int GetHashCode()
+        {
+            return this.ID;
+        }
+    }
+}
